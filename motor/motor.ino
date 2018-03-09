c01458f17ec44d48b8b12098dc38bc4793ec61bf
Andrei Pirvan <pirvandrei@gmail.com>
2018-03-09 12:05:31 +0100

    running code

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,44 +1,60 @@
 
 int motorASpeed = 1023;
-bool motorAForward = 0; 
+
+
+#define motor_a_speed 5
+#define motor_a_dir 0
+
+#define motor_b_speed 4
+#define motor_b_dir = 2
 
 void setup() {
   // Debug console
   Serial.begin(9600);
 
   //motor spped
-  pinMode(4, OUTPUT);
-  pinMode(0, OUTPUT);
 
-  //motor direction
-  pinMode(5, OUTPUT); 
-  pinMode(2, OUTPUT); 
+  // motor A
+  pinMode(motor_a_speed, OUTPUT);  // speed
+  pinMode(motor_a_dir, OUTPUT);
+
 }
 void loop() {
-  //spin out 
 
-   run(); 
-
-  delay(10000);
-   stop();
-   
-} 
-void stop(){
-  analogWrite(4, 0);
-    digitalWrite(2, motorAForward);
-
- 
-   analogWrite(5, 0);
-   digitalWrite(0, motorAForward); 
-    
-  }
+  // move forward
+  analogWrite(motor_a_speed, motorASpeed);
+  digitalWrite(motor_a_dir, 1);
+  delay(3000);
   
-void run(){
-  analogWrite(4, motorASpeed);
-    digitalWrite(2, motorAForward);
+  // stop
+  analogWrite(motor_a_speed, 0);
+  delay(3000);
+  
+  // backwards
+  analogWrite(motor_a_speed, motorASpeed);
+  digitalWrite(motor_a_dir, 0);
+  delay(3000);
 
- 
-   analogWrite(5, motorASpeed);
-   digitalWrite(0, motorAForward); 
-    
-  }
+
+}
+
+
+void stop() {
+ // analogWrite(4, 0);
+ // digitalWrite(2, motorAForward);
+
+  /*
+    analogWrite(5, 0);
+    digitalWrite(0, motorAForward); */
+
+}
+
+void run() {
+ //7 analogWrite(4, motorASpeed);
+ // digitalWrite(2, motorAForward);
+
+
+  // analogWrite(5, motorASpeed);
+  // digitalWrite(0, motorAForward);
+
+}
